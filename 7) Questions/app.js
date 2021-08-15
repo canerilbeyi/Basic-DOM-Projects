0985e66085bf5dc6772a8aaadc2cae84c8f76ebf
canerilbeyi <73815030+canerilbeyi@users.noreply.github.com>
2021-08-15 15:18:34 +0300

    Add files via upload

---
+++ 7) Questions/app.js
@@ -0,0 +1,30 @@
+//using selectors inside the element
+// traversing the dom
+
+// const questions = document.querySelectorAll(".question");
+
+// questions.forEach(function (question) {
+//   const btn = question.querySelector(".question-btn");
+//   btn.addEventListener("click", function () {
+//     questions.forEach(function (item) {
+//       console.log(item);
+//       if (item !== question) {
+//         item.classList.remove("show-text");
+//       }
+//     });
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+const btn = document.querySelectorAll(".question-btn");
+const questions = document.querySelectorAll(".question");
+
+btn.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    questions.forEach(function (question) {
+      if (question !== e.currentTarget.parentElement.parentElement)
+        question.classList.remove("show-text");
+    });
+    e.currentTarget.parentElement.parentElement.classList.toggle("show-text");
+  });
+});
